a58d308636a4d093ebd64210ae4866feaf475e10
城池 <xiaoming.dxm@alibaba-inc.com>
2017-02-28 19:49:17 +0800

    update

--- SwiftJavascriptCore/ViewController.swift
+++ SwiftJavascriptCore/ViewController.swift
@@ -1,25 +1,87 @@
 //
 //  ViewController.swift
-//  SwiftJavascriptCore
+//  SwiftJavaScriptCore
 //
-//  Created by 城池 on 2017/2/24.
-//  Copyright © 2017年 城池. All rights reserved.
+//  Created by myl on 16/6/8.
+//  Copyright © 2016年 Mayanlong. All rights reserved.
 //
-
 import UIKit
+import JavaScriptCore
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+@objc protocol JavaScriptMethodProtocol: JSExport {
+    var value: String {get set}
+     func postContent()
+     func postContentNumber(value:String,number: String)
 }
 
+@objc class JavaScriptMethod : NSObject, JavaScriptMethodProtocol {
+    var value: String {
+        get { return ""}
+        set {          }
+    }
+     func postContent() {
+        //方法名postContent
+        print("postContent")
+    }
+    
+     func postContentNumber(value: String, number: String) {
+        //方法名postContentNumber
+    }
+}
+
+
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    var jsContext: JSContext! = JSContext()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addView()
+        
+ 
+        
+        //webView.stringByEvaluatingJavaScript(from: "alert()")
+//        jsContext.evaluateScript("function demo(){ console.log('test');return 1;}")
+//        jsContext.evaluateScript("var a = 2;")
+//        let value = jsContext.evaluateScript("demo()")
+//
+//        
+//        let a = jsContext.objectForKeyedSubscript("a")
+//        print(value!)
+//        print(a!)
+        
+        
+        
+        
+
+
+        
+    }
+    
+    func addView(){
+        let path = Bundle.main.path(forResource: "demo", ofType: "html")
+        let url = URL.init(fileURLWithPath: path!)
+        let requestUrl = URLRequest(url:url)
+        webView.loadRequest(requestUrl)
+        webView.delegate = self  //if not add this code，evaluatedScript wont work
+        
+        jsContext = webView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as! JSContext!
+        jsContext.setObject( JavaScriptMethod() ,forKeyedSubscript:"callSwift" as (NSCopying & NSObjectProtocol)!)
+       
+
+    }
+    
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        jsContext.evaluateScript("jsForSwift()")
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
